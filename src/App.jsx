11cd902e358a042ff6eecd98446595e5236efff9
paramaguru077 <gurucse077@gmail.com>
2024-12-30 22:29:03 +0530

    little change

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
 import React from 'react'
-import Accordian from './Compontent/Accordion'
-import Project3 from './Compontent/Start-Rating/Index'
-import ImageSlider from './Compontent/image-Slider'
-import LoadMoreData from './Compontent/load-more-button'
+import LoadMoreData from './load-more-button'
 import './App.css'
 const App = () => {
   return (
     <div>
-     {/* <Accordian/>
-      <Project3/>
-      <ImageSlider url={'https://picsum.photos/v2/list'} limit={'10'}/>*/}
-
+     
       <LoadMoreData/>
 
     </div>
